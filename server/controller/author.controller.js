7e6a759516ea67018529c3b2cdf90c9f4e319c7b
Gus Cramer <cramerg2@wwu.edu>
2020-05-22 14:55:53 -0700

    added delete author functionality to server

--- server/controller/author.controller.js
+++ server/controller/author.controller.js
@@ -4,17 +4,23 @@ module.exports.addAuthor = (req, res) => {
     const {name} = req.body;
     Author.create({name})
         .then(newAuthor => res.json( {author: newAuthor} ))
-        .catch(err => res.json( {message: "Error adding new author", error: err} ));
+        .catch(err => res.json( {message: "Error adding new author: ", error: err} ));
 };
 
 module.exports.findAllAuthors = (req, res) => {
     Author.find({})
         .then(authors => res.json(authors))
-        .catch(err => res.json({message: "Error finding all authors", error: err}));
+        .catch(err => res.json({message: "Error finding all authors: ", error: err}));
 };
 
 module.exports.findAuthorById = (req, res) => {
     Author.find({_id: req.params.id})
         .then(author => res.json(author))
-        .catch(err => res.json({message: "Error finding author by Id", error: err}));
+        .catch(err => res.json({message: "Error finding author by Id: ", error: err}));
 };
+
+module.exports.deleteAuthor = (req, res) => {
+    Author.findOneAndDelete({_id: req.params.id})
+        .then(author => res.json(author))
+        .catch(err => res.json({message: "Error deleting author: ", error: err}))
+}
--- server/routes/author.routes.js
+++ server/routes/author.routes.js
@@ -4,4 +4,5 @@ module.exports = app => {
     app.post("/api/authors", AuthorController.addAuthor);
     app.get("/api/authors", AuthorController.findAllAuthors);
     app.get("/api/authors/:id", AuthorController.findAuthorById);
+    app.delete("/api/authors/:id", AuthorController.deleteAuthor);
 }
