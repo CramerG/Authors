d9e3775d5f393795f70d057f0ebe05d36601a41e
Gus Cramer <cramerg2@wwu.edu>
2020-05-22 15:37:54 -0700

    added basic list of authors to main page

--- client/src/views/AuthorList.js
+++ client/src/views/AuthorList.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { TableContainer, Paper, Table, TableRow, TableHead, TableCell } from '@material-ui/core';
+import { TableContainer, Paper, Table, TableRow, TableHead, TableCell, TableBody } from '@material-ui/core';
 
 const AuthorList = props => {
+    const [authors, setAuthors] = useState();
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect( () => {
+        axios.get("http://localhost:8000/api/authors")
+            .then(res => {
+                setAuthors(res.data);
+                setLoaded(true);
+            })
+            .catch(res => {
+                console.log(res);
+            });
+    });
 
     return(
         <div>
@@ -14,6 +27,16 @@ const AuthorList = props => {
                             <TableCell>Actions Available</TableCell>
                         </TableRow>
                     </TableHead>
+                    <TableBody>
+                        {loaded && authors.map( (author, idx) => {
+                            return(
+                                <TableRow>
+                                    <TableCell>{author.name}</TableCell>
+                                    <TableCell>Edit, Delete</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
                 </Table>
             </TableContainer>
         </div>
